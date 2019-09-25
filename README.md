00bda16ef0cd9244261496d8ce0430b427c763e6
laukik-hase <laukik17@gmail.com>
2019-09-25 14:10:14 +0530

    Update Codes

---
+++ README.md
@@ -0,0 +1,11 @@
+# ROS-Workshop-2.0
+
+Official Repository for ROS-based Manipulator, implemented with ESP32 (with socket programming)
+
+Prerequisites: ROS, initaliased with a catkin_ws workspace; ESP-IDF
+
+Run the following command for a quick install. 
+
+```bash
+./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,27 @@
+cd ~/catkin_ws/src 
+catkin_create_pkg sra std_msgs rospy roscpp
+
+cd ~/catkin_ws
+catkin_make
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+. ~/catkin_ws/devel/setup.bash
+
+rospack depends1 sra
+roscd sra
+cat package.xml
+
+cd ~/catkin_ws/src/sra
+rmdir -r include msg src 
+rm -f CMakelists.txt package.xml
+cd ~/ROS_Workshop-2.0
+cp msg scripts CMakelists.txt package.xml ~/catkin_ws/src/sra
+
+cd ~/catkin_ws
+catkin_make
+cd ~/catkin_ws/devel/sra/scripts
+sudo chmod +x Publisher.py Publisher_ForwardKinematics.py Publisher_InverseKinematics.py Subscriber.py SubscriberSocketTCP.py
+
+cd ~/ROS_Workshop-2.0
+cp -r ROS_SRA19 ~/esp/esp-idf/components
+mkdir ~/esp/esp-idf/codes
+cp -r Server_with_Servo_Control Servo_Set_Zero Servo_Sweep TCP_Server ~/esp/esp-idf/codes
